b310f6cb3e74bd4a23fdcc77508142a0de89964b
Ostap Onyshkiv <prostoostap58@gmail.com>
2022-03-16 21:24:15 +0200

    add files

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_7.2.2/Lab_7.2.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int s = 0;
+			int a[][6]
+			{ {0,8,9,15,6,-5},
+				{4,54,9,99,6,45},
+				{4,8,9,3,1,-50},
+				{9,15,100,16,50,100},
+				{15,0,-100,45,54,8},
+				{9,15,-6,15,100,3}
+			};
+
+			int** arr = new int* [6];
+			for (int i = 0; i < 6; i++)
+			{
+				arr[i] = new int[6];
+			}
+
+			for (int i = 0; i < 6; i++)
+			{
+				for (int j = 0; j < 6; j++)
+				{
+					arr[i][j] = a[i][j];
+				}
+			}
+			Calc(arr,s, 6, 6,1);
+			Assert::AreEqual(s, 7);
+
+			for (int i = 0; i < 6; i++)
+			{
+				delete[] arr[i];
+			}
+			delete[] arr;
+
+		}
+	};
+}
